3b4097655afccae0866f4f3bbbd58fec0e1c0d3a
Bruno Oliveira <brunoalex_88@yahoo.com.br>
2019-06-10 22:01:02 -0300

    .

--- src/main/java/com/obruno/controller/MessageController.java
+++ src/main/java/com/obruno/controller/MessageController.java
@@ -4,18 +4,25 @@ import com.obruno.model.Message;
 import com.obruno.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
+@RequestMapping(value = "/message")
 public class MessageController {
 
     @Autowired
     private MessageService messageService;
 
-    @GetMapping(value = "/message")
-    public void produce() {
-        Message message = Message.builder().id(1L).text("Pohaaaa").build();
+    @PostMapping
+    public void produce(Message message) {
         messageService.produce(message);
+    }
+
+    @GetMapping
+    public Message consume() {
+        return messageService.consume();
     }
 
 }
--- src/main/java/com/obruno/service/MessageService.java
+++ src/main/java/com/obruno/service/MessageService.java
@@ -2,29 +2,66 @@ package com.obruno.service;
 
 import com.obruno.avro.MessageAvro;
 import com.obruno.model.Message;
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import io.confluent.kafka.serializers.*;
+import lombok.extern.java.Log;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.LongSerializer;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.Properties;
 
 @Service
+@Log
 public class MessageService {
     public static final String TOPIC = "queue.message.created";
 
     public void produce(Message message) {
-        Producer<Long, MessageAvro> producer = createProducer();
-        producer.send(new ProducerRecord(TOPIC, message.getId(), message));
+        Producer<Long, Message> producer = createProducer();
+        producer.send(new ProducerRecord(TOPIC, message.getId(), buildMessage(message)), (metadata, exception) -> {
+            if (exception != null) {
+                log.warning(String.format("Erro ao produzir mensagem: %s", exception.getLocalizedMessage()));
+            } else {
+                log.info(String.format("Puta que pariu...foiii: %s", message));
+            }
+        });
         producer.flush();
         producer.close();
     }
 
-    private static Producer<Long, MessageAvro> createProducer() {
+    public Message consume() {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        properties.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+        properties.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongDeserializer");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "MessageConsumer");
+
+        KafkaConsumer<Long, Message> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList(MessageService.TOPIC));
+        ConsumerRecords<Long, Message> records = consumer.poll(100);
+
+        for (ConsumerRecord<Long, Message> record: records) {
+            log.info(String.format("Key: %s\nValue: %s", record.key(), record.value()));
+
+            Message message = new Message(record.value().getId(), record.value().getText());
+            return message;
+        }
+        return null;
+    }
+
+    private MessageAvro buildMessage(Message message) {
+        return MessageAvro.newBuilder().setId(message.getId()).setText(message.getText()).build();
+    }
+
+    private static Producer<Long, Message> createProducer() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.ACKS_CONFIG, "all");
